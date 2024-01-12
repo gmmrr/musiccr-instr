2fab06aa7fb4055896341b2edb0eb6261a7295a2
Guanming <gmmrr@Guanmings-Macbook.local>
2024-01-13 02:34:42 +0800

    first commit

---
+++ component/light.py
@@ -0,0 +1,12 @@
+class Light():
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def turn_on(self):
+        self.state = True
+
+    def turn_off(self):
+        self.state = False
---
+++ data/music.py
@@ -0,0 +1,6 @@
+class Music():
+    def __init__(self):
+        self.music = None
+    
+    def get_music(self):
+        print("Playing music...")

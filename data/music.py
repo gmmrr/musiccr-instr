7ac23cfffd22832f680dd64fa739d8828f2f6f33
Guanming <guanming091429@gmail.com>
2024-01-14 12:03:59 +0800

    build up based class

--- data/music.py
+++ data/music.py
@@ -1,6 +1,39 @@
+from component import bluetooth, nfc
+
 class Music():
-    def __init__(self):
+    def __init__(self, bluetooth, nfc):
+        self.bt = bluetooth
+        self.nfc = nfc
+
+        self.reset()
+
+
+    def reset(self):
+        print("Music:Reset")
         self.music = None
-    
-    def get_music(self):
-        print("Playing music...")
+        self.music_order = []
+        self.music_section = []
+
+
+
+    # decode
+    def decode_btdata_to_music(self):
+        print("Music:Decode BT to music")
+
+    def decode_nfcdata_to_order(self):
+        print("Music:Decode NFC to order")
+
+    def decode_music_to_section(self):
+        print("Music:Decode music into sections")
+
+
+    # receive music from bt, and receive order from nfc, and save them in section form
+    def receive_music(self):
+        print("Music:Receive")
+        # self.music = self.decode_btdata_to_music(self.bt.get_data())
+        # self.music_order = self.decode_nfcdata_to_order(self.nfc.get_data())
+        # self.msuic_section = self.decode_music_to_section(self.music, self.music_order)
+
+
+    def get_music_section(self):
+        print("Music:Get music_section (send to Speaker)")

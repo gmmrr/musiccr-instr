bf891254bca6a07d52ef2cf5a39635d481cc9094
Guanming <gmmrr@Guanmings-Macbook.local>
2024-02-21 20:36:03 +0800

    add pygame

--- main.py
+++ main.py
@@ -20,60 +20,61 @@ is_music_updated = False
 val_volume = 0
 val_bpm = 0
 val_pitch = 0
+val_music = "s3m3.mp3"
 
 music= music.Music()
 
 
 
-def volume_knob_thread():
-    '''
-    done
-    '''
-    global is_working
-    global is_volume_updated
-
-    global val_volume
-
-    volume_knob = knob.VolumeKnob(clk_pin = -1, dt_pin = -1)
-
-    while is_working:
-        if volume_knob.update():
-            is_volume_updated = True
-            val_volume = volume_knob.get_state()
-
-
-def bpm_knob_thread():
-    '''
-    done
-    '''
-    global is_working
-    global is_bpm_updated
-
-    global val_bpm
-
-    bpm_knob = knob.BPMKnob(clk_pin = -1, dt_pin = -1)
-
-    while is_working:
-        if bpm_knob.update():
-            is_bpm_updated = True
-            val_bpm = bpm_knob.get_state()
+# def volume_knob_thread():
+#     '''
+#     done
+#     '''
+#     global is_working
+#     global is_volume_updated
+#
+#     global val_volume
+#
+#     volume_knob = knob.VolumeKnob(clk_pin = -1, dt_pin = -1)
+#
+#     while is_working:
+#         if volume_knob.update():
+#             is_volume_updated = True
+#             val_volume = volume_knob.get_state()
 
 
-def pitch_slider_thread():
-    '''
-    done
-    '''
-    global is_working
-    global is_pitch_updated
+# def bpm_knob_thread():
+#     '''
+#     done
+#     '''
+#     global is_working
+#     global is_bpm_updated
+#
+#     global val_bpm
+#
+#     bpm_knob = knob.BPMKnob(clk_pin = -1, dt_pin = -1)
+#
+#     while is_working:
+#         if bpm_knob.update():
+#             is_bpm_updated = True
+#             val_bpm = bpm_knob.get_state()
 
-    global val_pitch
 
-    pitch_slider = slider.PitchSlider(pin = -1)
-
-    while is_working:
-        if pitch_slider.update():
-            is_pitch_updated = True
-            val_pitch = pitch_slider.get_state()
+# def pitch_slider_thread():
+#     '''
+#     done
+#     '''
+#     global is_working
+#     global is_pitch_updated
+#
+#     global val_pitch
+#
+#     pitch_slider = slider.PitchSlider(pin = -1)
+#
+#     while is_working:
+#         if pitch_slider.update():
+#             is_pitch_updated = True
+#             val_pitch = pitch_slider.get_state()
 
 
 def music_thread():
@@ -83,17 +84,20 @@ def music_thread():
     global is_working
     global is_music_updated
 
-    global music
+    global music  # put it as global for usage in the future
+    global val_music
 
-    while is_working:
-        if is_bpm_updated or is_pitch_updated:
 
-            music.update(bpm=val_bpm, pitch=val_pitch)
-            time.sleep(0.2)
+    while True:
+        while is_working:
+            if is_bpm_updated or is_pitch_updated:
 
-            is_music_updated = True
-            is_bpm_updated = False
-            is_pitch_updated = False
+                val_music = music.update(bpm=val_bpm, pitch=val_pitch)
+                time.sleep(0.2)
+
+                is_music_updated = True
+                is_bpm_updated = False
+                is_pitch_updated = False
 
 
 def speaker_thread():
@@ -104,26 +108,32 @@ def speaker_thread():
     global is_music_updated
 
     global music
+    global val_music
 
-    speaker = audioamp.AudioAmp(pin = -1, music=music)
+    speaker = audioamp.AudioAmp(pin = -1, music=val_music)
 
-    while is_working:
-        if is_music_updated:
-            speaker.update(music)
-            is_music_updated = False
+    speaker.play()
+
+    while True:
+        while is_working:
+            if is_music_updated:
+                speaker.update(val_music)
+                is_music_updated = False
+
+        speaker.stop()
 
 
-def light_thread():
-    '''
-
-    '''
-    global is_working
-
-    light_left = light.Light(pin = -1)
-    light_right = light.Light(pin = -1)
-
-    light_left.turn_on()
-    light_right.turn_on()
+# def light_thread():
+#     '''
+#
+#     '''
+#     global is_working
+#
+#     light_left = light.Light(pin = -1)
+#     light_right = light.Light(pin = -1)
+#
+#     light_left.turn_on()
+#     light_right.turn_on()
 
 
 def play_button_thread():
@@ -134,6 +144,7 @@ def play_button_thread():
 
     while True:
         key = input("Press Spacebar to start/stop:")
+
         if key == ' ':
             is_working = not is_working
             if is_working:
@@ -152,30 +163,30 @@ def main():
     global is_music_updated
 
     print("Instrument: Start")
-    t_volume_knob = threading.Thread(target=volume_knob_thread)
-    t_bpm_knob = threading.Thread(target=bpm_knob_thread)
-    t_pitch_slider = threading.Thread(target=pitch_slider_thread)
+    # t_volume_knob = threading.Thread(target=volume_knob_thread)
+    # t_bpm_knob = threading.Thread(target=bpm_knob_thread)
+    # t_pitch_slider = threading.Thread(target=pitch_slider_thread)
     t_music = threading.Thread(target=music_thread)
     t_speaker = threading.Thread(target=speaker_thread)
-    t_light = threading.Thread(target=light_thread)  # actually it just turns on the light once
+    # t_light = threading.Thread(target=light_thread)  # actually it just turns on the light once
     t_play_button = threading.Thread(target=play_button_thread)
 
     # start threads
-    t_volume_knob.start()
-    t_bpm_knob.start()
-    t_pitch_slider.start()
+    # t_volume_knob.start()
+    # t_bpm_knob.start()
+    # t_pitch_slider.start()
     t_music.start()
     t_speaker.start()
-    t_light.start()
+    # t_light.start()
     t_play_button.start()
 
     # wait for threads to finish
-    t_volume_knob.join()
-    t_bpm_knob.join()
-    t_pitch_slider.join()
+    # t_volume_knob.join()
+    # t_bpm_knob.join()
+    # t_pitch_slider.join()
     t_music.join()
     t_speaker.join()
-    t_light.join()
+    # t_light.join()
     t_play_button.join()
 
     print("Instrument: End")

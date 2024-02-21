d0ae5aa52873804514caa6111ed747e7344587cd
Guanming <gmmrr@Guanmings-Macbook.local>
2024-02-21 11:49:25 +0800

    update knob's update function

--- component/knob.py
+++ component/knob.py
@@ -1,7 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+import math
+
+# Function
+def section(num):
+    if num >= 0:
+        return math.floor(num)
+    else:
+        return math.ceil(num)
+
+# Main Function
 class Knob():
-    def __init__(self, pin):
-        self.pin = pin
-        self.state = 0
+    def __init__(self, clk_pin, dt_pin, sw_pin):
+        self.clk_pin = Pin(clk_pin, Pin.IN, Pin.PULL_DOWN)
+        self.dt_pin = Pin(dt_pin, Pin.IN, Pin.PULL_DOWN)
+        self.sw_pin = Pin(sw_pin, Pin.IN, Pin.PULL_DOWN)
+
+        self.reset()
+
+
+
+    def reset(self):
+        self.deadzone = 4
+
+        self.counter = 0
+        self.idle_time = 0
+
+        self.clk_last_state = self.clk_pin.value() # in units of the number(counter)
+        self.current_state = 3 # in units of 1 to 5, and 3 is default
+        self.last_changing_point = 0
+
+
+
+
 
     def update(self):
         pass
@@ -11,81 +42,86 @@ class Knob():
 
 
 class VolumeKnob(Knob):
-    def __init__(self, pin):
+    def __init__(self, clk_pin, dt_pin, sw_pin):
         # derive from Slider class
-        super().__init__(pin)
-        self.state = 50
+        super().__init__(clk_pin, dt_pin, sw_pin)
+        self.deadzone = 1
+        self.current_state = 50
 
-class BPMKnob(Knob):
-    def __init__(self, pin):
-        # derive from Slider class
-        super().__init__(pin)
-        self.state = 3
+    def update(self):
+        clk_state = self.clk_pin.value()
+        dt_state = self.dt_pin.value()
 
-
-
-# ------------------------------------------------------------------
-from machine import Pin
-from time import sleep
-import math
-
-clk_pin = Pin(12, Pin.IN, Pin.PULL_DOWN)
-dt_pin = Pin(13, Pin.IN, Pin.PULL_DOWN)
-
-def section(num):
-    if num >= 0:
-        return math.floor(num)
-    else:
-        return math.ceil(num)
-
-try:
-    print("Rotary Encoder:Start")
-
-    deadzone = 4
-
-    counter = 0
-    idle_time = 0
-
-    clk_last_state = clk_pin.value() # in units of the number(counter)
-    current_state = 3 # in units of 1 to 5, and 3 is default
-    last_changing_point = 0
-
-    while True:
-        clk_state = clk_pin.value()
-        dt_state = dt_pin.value()
-
-        if clk_state != clk_last_state:
-            idle_time = 0 # start to change
+        if clk_state != self.clk_last_state:
+            self.idle_time = 0 # start to change
             if dt_state != clk_state:
-                counter += 1
+                self.counter += 1
             else:
-                counter -= 1
+                self.counter -= 1
 
         else: # deal with the idle time
-            idle_time += 1
-            if idle_time == 300:
-                print("Rotary Encoder:Idle")
-                counter = 0
-                last_changing_point = 0
+            self.idle_time += 1
+            if self.idle_time == 300:
+                self.counter = 0
+                self.last_changing_point = 0
 
-       
-        if section(counter/deadzone) > last_changing_point:
-            if current_state < 5:
-                current_state += 1
-            last_changing_point = section(counter/deadzone)
-        elif section(counter/deadzone) < last_changing_point:
-            if current_state > 1:
-                current_state -= 1
-            last_changing_point = section(counter/deadzone)
 
-        if idle_time < 300:
-            print(current_state)    
-            
+        if section(self.counter/self.deadzone) > self.last_changing_point:
+            if self.current_state < 100:
+                self.current_state += 1
+            self.last_changing_point = section(self.counter/self.deadzone)
+        elif section(self.counter/self.deadzone) < self.last_changing_point:
+            if self.current_state > 0:
+                self.current_state -= 1
+            self.last_changing_point = section(self.counter/self.deadzone)
 
-        clk_last_state = clk_state
-        sleep(0.01)
-        
-except KeyboardInterrupt:
-    print("Rotary Encoder:Stop")
-finally:
-    pass
+        if self.idle_time < 300:
+            print(self.current_state)
+
+
+        self.clk_last_state = self.clk_state
+        time.sleep(0.01)
+
+
+
+
+class BPMKnob(Knob):
+    def __init__(self, clk_pin, dt_pin, sw_pin):
+        # derive from Slider class
+        super().__init__(clk_pin, dt_pin, sw_pin)
+        self.deadzone = 4
+        self.current_state = 3
+
+    def update(self):
+        clk_state = self.clk_pin.value()
+        dt_state = self.dt_pin.value()
+
+        if clk_state != self.clk_last_state:
+            self.idle_time = 0 # start to change
+            if dt_state != clk_state:
+                self.counter += 1
+            else:
+                self.counter -= 1
+
+        else: # deal with the idle time
+            self.idle_time += 1
+            if self.idle_time == 300:
+                self.counter = 0
+                self.last_changing_point = 0
+
+
+        if section(self.counter/self.deadzone) > self.last_changing_point:
+            if self.current_state < 5:
+                self.current_state += 1
+            self.last_changing_point = section(self.counter/self.deadzone)
+        elif section(self.counter/self.deadzone) < self.last_changing_point:
+            if self.current_state > 1:
+                self.current_state -= 1
+            self.last_changing_point = section(self.counter/self.deadzone)
+
+        if self.idle_time < 300:
+            print(self.current_state)
+
+
+        self.clk_last_state = self.clk_state
+        time.sleep(0.01)

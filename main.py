5cc9e62cee899cb971f86c4f5e0191c418b7c845
Guanming <guanming091429@gmail.com>
2024-02-24 21:31:46 +0800

    remove one light

--- main.py
+++ main.py
@@ -143,11 +143,9 @@ def light_thread():
     '''
 
     global is_working
-    light_left = light.Light(pin = 18)
-    light_right = light.Light(pin = 33)
+    light = light.Light(pin = 18)
 
-    light_left.turn_on()
-    light_right.turn_on()
+    light.turn_on()
 
 
 

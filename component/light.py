979c8a70220ed0739d00ec1dc2853dc1175fc5f7
Guanming <gmmrr@Guanmings-Macbook.local>
2024-02-25 13:08:49 +0800

    debug

--- component/light.py
+++ component/light.py
@@ -20,7 +20,15 @@ class Light():
         self.LED_INVERT = False # True to invert the signal (when using NPN transistor level shift)
         self.LED_CHANNEL = 0 # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-        self.strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT,self.LED_BRIGHTNESS, self.LED_CHANNEL)
+        self.strip = Adafruit_NeoPixel(
+            self.LED_COUNT,
+            self.LED_PIN,
+            self.LED_FREQ_HZ,
+            self.LED_DMA,
+            self.LED_INVERT,
+            self.LED_BRIGHTNESS,
+            self.LED_CHANNEL
+        )
         self.strip.begin()
 
 

c3f4459765b61e2479b228e7e917efde2f48cbb0
Guanming <guanming091429@gmail.com>
2024-02-25 12:59:32 +0800

    change way to control led strip

--- component/light.py
+++ component/light.py
@@ -1,5 +1,5 @@
 import time
-from rpi_ws281x import PixelStrip, Color
+from rpi_ws281x import *
 
 
 class Light():
@@ -10,23 +10,28 @@ class Light():
 
 
     def init(self):
-        LED_COUNT = 8
-        LED_PIN = self.pin
-        LED_FREQ_HZ = 800000
-        LED_DMA = 10  # DMA 通道
-        LED_BRIGHTNESS = 255
-        LED_INVERT = False
+        from rpi_ws281x import *
 
-        self.strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-        self.strip.begin()
+        # LED strip configuration:
+        self.LED_COUNT = 300 # Number of LED pixels.
+        self.LED_PIN = 18 # GPIO pin connected to the pixels (18 uses PWM!).
+        # self.LED_PIN = 10 # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+        self.LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
+        self.LED_DMA = 10 # DMA channel to use for generating signal (try 10)
+        self.LED_BRIGHTNESS = 255 # Set to 0 for darkest and 255 for brightest
+        self.LED_INVERT = False # True to invert the signal (when using NPN transistor level shift)
+        self.LED_CHANNEL = 0 # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+        strip = Adafruit_NeoPixel(self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ, self.LED_DMA, self.LED_INVERT,self.LED_BRIGHTNESS, self.LED_CHANNEL)
+        strip.begin()
 
 
     def turn_on(self):
-        for i in range(self.strip.numPixels()):
+        for i in range(0,self.LED_COUNT):
             self.strip.setPixelColor(i, Color(255, 255, 255))
         self.strip.show()
 
     def turn_off(self):
-        for i in range(self.strip.numPixels()):
+        for i in range(0,self.LED_COUNT):
             self.strip.setPixelColor(i, Color(0, 0, 0))
         self.strip.show()

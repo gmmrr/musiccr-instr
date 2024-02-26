37cf519dd02110f5870c5867728a88d954e298a2
Guanming <gmmrr@Guanmings-Macbook.local>
2024-02-27 00:03:36 +0800

    change pin

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def pitch_slider_thread():
     global is_pitch_updated
     global val_pitch
 
-    pitch_slider = slider.PitchSlider(pin = 18)
+    pitch_slider = slider.PitchSlider(pin = 2)
 
     counter = 0
 

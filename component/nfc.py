0b9694bb5636e12ae30c4cc09213b0c0a85466be
Guanming <gmmrr@Guanmings-Macbook.local>
2024-03-21 18:35:43 +0800

    change the model of nfc

--- component/nfc.py
+++ component/nfc.py
@@ -1,15 +1,7 @@
-from api import MFRC522
-import signal
-
-# Capture SIGINT for cleanup when the script is aborted
-def end_read(signal,frame):
-    print("Ctrl+C captured, ending read.")
 
 class NFC:
     def __init__(self, pin):
         self.pin = pin
-        self.MIFAREReader = MFRC522.MFRC522()
-        signal.signal(signal.SIGINT, end_read)
 
     def read(self):
         '''
@@ -20,20 +12,3 @@ class NFC:
         Returns:
             data (str): data read from NFC tag
         '''
-        # return self.nfc_reader.read()
-        #!/usr/bin/env python
-        # Scan for cards
-        (status,TagType) = self.MIFAREReader.MFRC522_Request(self.MIFAREReader.PICC_REQIDL)
-
-        # If a card is found
-        if status == self.MIFAREReader.MI_OK:
-            print("Card detected")
-
-        # Get the UID of the card
-        (status,uid) = self.MIFAREReader.MFRC522_Anticoll()
-
-        # If we have the UID, continue
-        if status == self.MIFAREReader.MI_OK:
-
-            # print(UID
-            print("Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))

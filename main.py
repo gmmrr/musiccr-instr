3344c7bd73dee297b5c995fb6fa6cc35a9970fcd
Guanming <gmmrr@Guanmings-Macbook.local>
2024-03-22 00:15:49 +0800

    test

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import threading
 import RPi.GPIO as GPIO
 import time
 
-from mfrc522 import SimpleMFRC522
+# from mfrc522 import SimpleMFRC522
+from pirc522 import RFID
+
 
 
 # ------------------------------
@@ -258,36 +260,34 @@ def nfc_thread():
 
 def read_rfid():
 
-    reader = SimpleMFRC522()
+    # reader = SimpleMFRC522()
+
+    rdr = RFID()
 
     try:
+      while True:
+        rdr.wait_for_tag()
+        (error, tag_type) = rdr.request()
+        if not error:
+          print("Tag detected")
+          (error, uid) = rdr.anticoll()
+          if not error:
+            print("UID: " + str(uid))
+            # Select Tag is required before Auth
+            if not rdr.select_tag(uid):
+              # Auth for block 10 (block 2 of sector 2) using default shipping key A
+              if not rdr.card_auth(rdr.auth_a, 10, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF], uid):
+                # This will print something like (False, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+                print("Reading block 10: " + str(rdr.read(10)))
+                # Always stop crypto1 when done working
+                rdr.stop_crypto()
 
-        print("test1")
+        # rdr.wait_for_tag()
+        # uid = rdr.read_id(as_number = True)
+        # if uid is not None:
+        #     print(f'UID: {uid:X}')
 
-        reader = SimpleMFRC522()
-
-        print("test2")
-
-        # print("Place card to write: ")
-        # text = "Hello, world"
-        # id, text_written = reader.write(text)
-        # print(id)
-        # print(text_written)
-        # print(f"Written.")
-
-        print("test3")
-
-        time.sleep(3)
-        print("Place your card to read: ")
-        id, text = reader.read()
-        print(id)
-        print(text)
-        print("Read.")
-
-        print("test4")
-
-
-    finally:
+    except KeyboardInterrupt:
         GPIO.cleanup()
 
 
@@ -336,9 +336,7 @@ def main():
 
     print("Instrument: End")
 
-    while True:
-        card_id = read_rfid()
-        print("RFID Card ID:", card_id)
+    read_rfid()
 
 
 

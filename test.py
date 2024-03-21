348273b3ba55a6ff94e7949025a354c4b56972ec
Guanming <gmmrr@Guanmings-Macbook.local>
2024-03-21 20:29:15 +0800

    test

--- test.py
+++ test.py
@@ -1,16 +1,30 @@
+# from mfrc522 import SimpleMFRC522
+#
+# reader = SimpleMFRC522()
+#
+# print("test")
+#
+# text = "Hello, world"
+# id, text_written = reader.write(text)
+# print("test")
+# print(f"ID: {id}")
+# print(f"Text Written: {text_written}")
+#
+# id, text = reader.read()
+# print("test")
+# print(f"ID: {id}")
+# print(f"Text: {text}")
+
+import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
-print("test")
+try:
+        text = input('New data:')
+        print("Now place your tag to write")
+        reader.write(text)
+        print("Written")
 
-text = "Hello, world"
-id, text_written = reader.write(text)
-print("test")
-print(f"ID: {id}")
-print(f"Text Written: {text_written}")
-
-id, text = reader.read()
-print("test")
-print(f"ID: {id}")
-print(f"Text: {text}")
+finally:
+        GPIO.cleanup()

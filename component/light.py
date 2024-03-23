6e6b818999ebbd1d5cd26ad980c96f1000e7c584
Guanming <gmmrr@Guanmings-Macbook.local>
2024-03-23 16:09:54 +0800

    rm light

--- component/light.py
+++ component/light.py
@@ -65,3 +65,47 @@ class Light():
         for i in range(0,self.LED_COUNT):
             self.strip.setPixelColor(i, Color(0, 0, 0))
         self.strip.show()
+
+
+
+
+
+
+#------------------------------------
+# this should be put inside main.py
+#------------------------------------
+
+# def light_thread():
+#     '''
+#     To deal with the light effect
+#
+#     '''
+#
+#     global is_working
+#     global is_music_updated
+#     global is_volume_updated
+#     global val_music
+#     global val_volume
+#     global pin_led
+#
+#
+#     light_obj = light.Light(pin = pin_led)
+#     light_obj.update(val_music, val_volume)
+#
+#     # while True:
+#     #     while is_working:
+#     #
+#     #         if is_music_updated or is_volume_updated:
+#     #             light.update(val_music, val_volume)
+#     #             time.sleep(0.05)
+#     #             is_music_updated = False
+#     #             is_volume_updated = False
+#
+#     t_light_turn_on = threading.Thread(target=light_obj.turn_on)
+#     t_light_turn_on.start()
+#
+#     t_light_turn_on = threading.Thread(target=light_obj.turn_on)
+#     t_light_turn_on.start()
+#
+#
+#         # light_obj.turn_off()

71695cf683b1ff1fe5241e2185b047fe46d3bed4
gmmrr <guanming091429@gmail.com>
2024-04-02 18:11:53 +0800

    add nfc

--- component/nfc.py
+++ component/nfc.py
@@ -1,6 +1,15 @@
+from api import MFRC522
+import signal
+
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal,frame):
+    print("Ctrl+C captured, ending read.")
+
 class NFC:
     def __init__(self, pin):
         self.pin = pin
+        self.MIFAREReader = MFRC522.MFRC522()
+        signal.signal(signal.SIGINT, end_read)
 
     def read(self):
         '''
@@ -12,13 +21,19 @@ class NFC:
             data (str): data read from NFC tag
         '''
         # return self.nfc_reader.read()
+        #!/usr/bin/env python
+        # Scan for cards
+        (status,TagType) = self.MIFAREReader.MFRC522_Request(self.MIFAREReader.PICC_REQIDL)
 
-    def write(self, data):
-        '''
-        Write data to NFC tag
+        # If a card is found
+        if status == self.MIFAREReader.MI_OK:
+            print("Card detected")
 
-        Args:
-            data (str): data to write to NFC tag
-        Returns:
-            None
-        '''
+        # Get the UID of the card
+        (status,uid) = self.MIFAREReader.MFRC522_Anticoll()
+
+        # If we have the UID, continue
+        if status == self.MIFAREReader.MI_OK:
+
+            # print(UID
+            print("Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3]))

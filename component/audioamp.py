e22d7e441a5252264276aa4aae7f61e6c2233cc8
gmmrr <guanming091429@gmail.com>
2024-04-02 18:11:49 +0800

    add pygame

--- component/audioamp.py
+++ component/audioamp.py
@@ -1,18 +1,46 @@
 from machine import Pin
 from api import music
 
+import pygame
+
+
 class AudioAmp():
-    def __init__(self, pin, music):
+    def __init__(self, pin, val_music):
         self.pin = Pin(pin, Pin.OUT)
-        self.music = music
+        self.music = val_music
+
+
+    def reset(self):
+        pygame.mixer.init()
 
 
     def update(self, music):
         '''
         Args:
-        - music: to update the music
+        - music (string): to update the music
 
         Returns:
         - void
         '''
         self.music = music
+        pygame.mixer.music.load(self.music)
+
+    def play(self):
+        '''
+        play the music
+        Args:
+        -
+        Returns:
+        - void
+        '''
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            continue
+
+    def stop(self):
+        pygame.mixer.music.stop()
+
+
+
+# the volume
+# pygame.mixer.music.set_volume(0.5)

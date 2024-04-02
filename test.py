1ceabf4ab34e35c5e12f8c2b7326f3ef3d91ccfa
gmmrr <guanming091429@gmail.com>
2024-04-02 18:11:54 +0800

    test

--- test.py
+++ test.py
@@ -4,6 +4,11 @@ reader = SimpleMFRC522()
 
 print("test")
 
+text = "Hello, world"
+id, text_written = reader.write(text)
+print(f"ID: {id}")
+print(f"Text Written: {text_written}")
+
 id, text = reader.read()
 print("test")
 print(f"ID: {id}")
